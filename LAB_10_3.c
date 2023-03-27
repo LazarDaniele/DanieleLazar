bb98a9c9937e197e931394d75645cdb751432c5d
LazarDaniele <93709347+LazarDaniele@users.noreply.github.com>
2023-03-27 08:31:44 +0200

    Add files via upload

---
+++ LAB_10_3.c
@@ -0,0 +1,13 @@
+	
+
+public class ContaNani2 extends Thread {
+
+    public ContaNani2(String nome) {
+        super();
+        setName(nome);
+    }
+    public void run(){
+        for(int i=0; i<7; i++)
+            System.out.println((i+1)+" "+getName());
+    }
+}
